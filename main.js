d96a6b8ef9abbca904828bf931628325ff49d14e
Richard Conway <richard@rconway.co.uk>
2019-12-09 20:21:17 +0000

    Move images into dedicated directory.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+const IMAGE_DIR = "./images"
+
 Vue.component("product", {
     props: {
         premium: {
@@ -51,13 +54,13 @@ Vue.component("product", {
                 {
                     variantId: 2234,
                     variantColor: "green",
-                    variantImage: "./vmSocks-green-onWhite.jpg",
+                    variantImage: "vmSocks-green-onWhite.jpg",
                     variantQuantity: 10
                 },
                 {
                     variantId: 2235,
                     variantColor: "blue",
-                    variantImage: "./vmSocks-blue-onWhite.jpg",
+                    variantImage: "vmSocks-blue-onWhite.jpg",
                     variantQuantity: 0
                 }
             ],
@@ -81,7 +84,7 @@ Vue.component("product", {
             return this.brand + " " + this.product
         },
         image() {
-            return this.variants[this.selectedVariant].variantImage
+            return IMAGE_DIR + "/" + this.variants[this.selectedVariant].variantImage
         },
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity > 0
